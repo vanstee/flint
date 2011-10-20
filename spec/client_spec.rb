32d0a8b21777478bbbec13a20b30ba392e65f681
vanstee <vanstee@highgroove.com>
2011-10-19 22:12:02 -0400

    Write tests for responding to messages with blocks and guards

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -14,4 +14,39 @@ describe Flint::Client do
       @client.state.should == :initializing
     end
   end
+
+  describe '#message' do
+    before do
+      @client = Flint::Client.new
+      @response = mock()
+    end
+
+    it 'can be a hash with string match' do
+      @response.should_receive(:call)
+      @client.register_handler(:message, :body => "Taco party?") { @response.call }
+
+      @client.handle_message(:body => "Taco party?")
+    end
+
+    it 'can be a hash with a regexp' do
+      @response.should_receive(:call)
+      @client.register_handler(:message, :body => /tacos/) { @response.call }
+
+      @client.handle_message(:body => "So we\'re going to eat tacos for lunch 4 days in a row")
+    end
+
+    it 'can be a hash with an array' do
+      @response.should_receive(:call)
+      @client.register_handler(:message, :body => ["Taqueria del Sol", "Taco Bell", "Del Taco"]) { @response.call }
+
+      @client.handle_message(:body => "Taqueria del Sol")
+    end
+
+    it 'can be a hash with a lambda' do
+      @response.should_receive(:call)
+      @client.register_handler(:message, :body => lambda { |body| body.length == 30 }) { @response.call }
+
+      @client.handle_message(:body => "What\'s the blue plate special?")
+    end
+  end
 end
